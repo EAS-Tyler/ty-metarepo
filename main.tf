0f7c2e7e4e7b929968a55921ad74c77aee728aaa
EAS-Tyler <tyler@enterpriseautomation.co.uk>
2025-07-08 09:20:30 +0100

    rename

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "tyler-module-test" {
   source                 = "./RVM"
-  repository_name        = "tyler-module-test44"
-  repository_description = "tyler-module-test44"
+  repository_name        = "tyler-module-test"
+  repository_description = "tyler-module-test"
   pat                    = var.pat
 }
