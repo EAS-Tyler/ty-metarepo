f6883b9e59224a3a7988e5008dc5de0f60027e77
EAS-Tyler <tyler@enterpriseautomation.co.uk>
2025-07-08 20:29:18 +0100

    ty2

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "tyler-module-test" {
   source                 = "./RVM"
-  repository_name        = "tyler-module-test"
-  repository_description = "tyler-module-test"
+  repository_name        = "tyler-test"
+  repository_description = "tyler-test"
   pat                    = var.pat
   github_token           = var.github_token
 }
