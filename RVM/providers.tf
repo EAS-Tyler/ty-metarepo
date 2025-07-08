ecf17b240846b00caf0aa081b4a73369a305a167
EAS-Tyler <tyler@enterpriseautomation.co.uk>
2025-07-08 14:58:18 +0100

    child-mod-providers

--- RVM/providers.tf
+++ RVM/providers.tf
@@ -6,28 +6,14 @@ terraform {
       source  = "integrations/github"
       version = "6.6.0"
     }
-
     harness = {
       source  = "harness/harness"
       version = "0.37.5"
     }
-
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "2.23.0"
     }
-
   }
-
-  # backend "gcs" {
-  #   bucket = "easinfra-tfstate-bucket"
-  #   prefix = "terraform/state/ty-demo"
-  # }
 }
-
-# provider "kubernetes" {
-#   config_path    = "~/.kube/config"
-#   config_context = "minikube"
-# }
-
 
